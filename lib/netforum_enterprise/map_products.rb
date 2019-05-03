749e321d1d276d2cf29729163226f5fdc2872bcb
Elochai <buu9992@gmail.com>
2019-05-03 12:45:15 +0300

    update get_events_registrant_by_event_key

--- lib/netforum_enterprise/map_products.rb
+++ lib/netforum_enterprise/map_products.rb
@@ -101,9 +101,9 @@ module NetforumEnterprise
       where_clause << " and reg_cancel_date is null and reg_delete_flag='0'" if search_for_actual_events
       get_array('get_query', {
         'szObjectName' => 'EventsRegistrant',
-        'szColumnList' => return_list || "cst_id,Registrant.reg_cst_key AS Registrant_reg_cst_key,Registrant.reg_evt_key AS Registrant_reg_evt_key,Registrant.reg_registration_date AS Registrant_reg_registration_date",
+        'szColumnList' => return_list || "ivd_key,cst_id,Registrant.reg_cancel_date AS Registrant_reg_cancel_date,Registrant.reg_cst_key AS Registrant_reg_cst_key,Registrant.reg_evt_key AS Registrant_reg_evt_key,Registrant.reg_registration_date AS Registrant_reg_registration_date",
         'szWhereClause' => "#{where_clause}",
-        'szOrderBy' => ''
+        'szOrderBy' => 'Registrant.reg_registration_date DESC'
       }, Registrant, { output_subname: 'events_registrant_object' })
     end
 
