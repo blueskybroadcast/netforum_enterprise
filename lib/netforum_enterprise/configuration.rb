68fdd20cb5b2364f0ec5a54247325746dc37f22a
Elochai <buu9992@gmail.com>
2020-05-19 16:23:00 +0300

    Add proxy option from provider

--- lib/netforum_enterprise/configuration.rb
+++ lib/netforum_enterprise/configuration.rb
@@ -1,7 +1,7 @@
 module NetforumEnterprise
   class Configuration
-    attr_accessor :provider, :client_options
-    attr_writer :wsdl, :use_execute_method
+    attr_accessor :provider
+    attr_writer :wsdl, :use_execute_method, :client_options
 
     def initialize
       logger = defined?(Rails) ? Rails.logger : Logger.new(STDOUT)
@@ -9,6 +9,7 @@ module NetforumEnterprise
       @wsdl = nil
       @use_execute_method = false
       @client_options = { log: true, log_level: :debug, logger: logger, pretty_print_xml: true }
+      @client_options[:proxy] = proxy_url if use_proxy?
     end
 
     def wsdl
@@ -17,6 +18,16 @@ module NetforumEnterprise
 
     def use_execute_method?
       @provider.settings&.dig('use_execute_method').present? || @use_execute_method
+    end
+
+    private
+
+    def proxy_url
+      ENV['NETFORUM_ENTERPRISE_PROXY_URL'].presence || ENV['FIXIE_URL']
+    end
+
+    def use_proxy?
+      @provider.settings&.dig('use_proxy').present?
     end
   end
 end
